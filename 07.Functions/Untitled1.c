872a03aa49673dff062db218efa63f143352f911
AliyenSha <zihadshai701@gmail.com>
2024-03-29 23:28:18 +0600

    Preparation for final of spring24 started by Function previous all was for Mid and Fall23

---
+++ 07.Functions/Untitled1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+  void printMessage()
+    {
+        printf("This is a function\n");
+    }
+
+int main()
+    {
+        printMessage();
+        printMessage();
+        printMessage();
+        printMessage();
+    }
+
+
